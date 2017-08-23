4e10f82453c90dbddc57611d02119cd11733a98f
Mark Franciose <mark.franciose@gmail.com>
2017-08-23 11:31:40 -0500

    Revert "Delete formatted date"

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,10 @@
 class Reminder < ApplicationRecord
   belongs_to :habit
   validates_presence_of :habit_id, :answer
+  after_create :created_at_format
+
+  def created_at_format
+    self.formatted_date = self.created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
 
 end
--- db/migrate/20170823152824_delete_formatted_date.rb
+++
@@ -1,5 +0,0 @@
-class DeleteFormattedDate < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :reminders, :formatted_date, :string
-  end
-end
