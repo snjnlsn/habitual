c55ead7bcc53bb77c409fa4589c52623f36c4dd5
Mark Franciose <mark.franciose@gmail.com>
2017-08-24 07:08:49 -0500

    name changing for bug fixes

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -1,4 +1,4 @@
-class RandomController < ApplicationController
+class RandomsController < ApplicationController
 
   def random
     @habit = Habit.new(habit_params)
@@ -13,6 +13,6 @@ class RandomController < ApplicationController
   private
 
   def habit_params
-    params.require(:habit).permit(:name, :reminder_frequency, :reminder_time)
+    params.require(:habit).permit(:name, :reminder_frequency, :reminder_time, :end_time)
   end
 end
